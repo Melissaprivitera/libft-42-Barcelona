2aeeac2f8d500f14d470ca2e0333124b8adaf528
Melissaprivitera <101509873+Melissaprivitera@users.noreply.github.com>
2023-01-11 17:08:17 +0100

    version final de ft_isalpha.c
    version con el main (ojo)

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,33 @@
 /*   By: melprivi <melprivi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:06:01 by melprivi          #+#    #+#             */
-/*   Updated: 2023/01/11 16:07:53 by melprivi         ###   ########.fr       */
+/*   Updated: 2023/01/11 16:53:25 by melprivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int ft_isalpha(int c);
+
+int main()
+{
+    if (ft_isalpha('a')) 
+    {
+        printf("'A' is an alphabetic character.\n");
+    } 
+    else 
+    {
+        printf("'A' is not an alphabetic character.\n");
+    }
+} 
+
+int ft_isalpha(int c)
+{
+    int bool;
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        bool = 1;
+    else
+        bool = 0;
+    return (bool);
+}
