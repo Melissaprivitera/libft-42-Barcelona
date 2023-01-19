56567b063a17f98ef02752aabe8d836da1bd35bb
Melissaprivitera <101509873+Melissaprivitera@users.noreply.github.com>
2023-01-19 20:23:17 +0100

    correct files

--- testfunctions.c
+++ testfunctions.c
@@ -6,18 +6,20 @@
 /*   By: melprivi <melprivi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:01:24 by melprivi          #+#    #+#             */
-/*   Updated: 2023/01/18 19:41:14 by melprivi         ###   ########.fr       */
+/*   Updated: 2023/01/19 20:23:16 by melprivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
     int		i;
     int     a;
+    int     b;
     char    *str;
 
 		
@@ -28,10 +30,10 @@ int main(int argc, char **argv)
 		{
             printf(" the input to evaluate is %s ", argv[i]);
 			str = argv[i];
-            a = *str - '0';
+            b = (int)*str - '0';
             printf(" the input is %d\n", a); 
-            printf("the result for %c from my function ft_isalpha is %d ", *str, ft_isalpha(a));
-            printf("the result for %c from the standard isalpha library is %d\n", *str, isalpha(a));
+            printf("the result for %c for ft_isalpha() is %d and for isalpha() is %d\n", b, ft_isalpha(b), isalpha(b));
+            printf("the result for %c for ft_isdigit() is %d and for isdigit() is %d\n", b, ft_isdigit(b), isdigit(b));
             i++;
 		}
 	}
@@ -41,14 +43,10 @@ int main(int argc, char **argv)
         i = 0;
         while (i < 128)
         {
-            if (isalpha(i))
-            {
-                printf("the result for %c from my function ft_isalpha is %d ", i, ft_isalpha(i));
-                printf("the result for %c from the standard isalpha library is %d\n", i, isalpha(i));
-            }
+            printf("the result for %c for ft_isalpha() is %d and for isalpha() is %d\n", i, ft_isalpha(i), isalpha(i));
+            printf("the result for %c for ft_isdigit() is %d and for isdigit() is %d\n", i, ft_isdigit(i), isdigit(i));
             i++;
         }
-
     }        
     return (0);
 }
