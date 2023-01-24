35380144c567651301f0f082ff6d4611bc840693
Melissaprivitera <101509873+Melissaprivitera@users.noreply.github.com>
2023-01-24 23:02:03 +0100

    added memcpy norminnetted

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,27 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: melprivi <melprivi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/19 21:07:09 by melprivi          #+#    #+#             */
-/*   Updated: 2023/01/24 22:33:19 by melprivi         ###   ########.fr       */
+/*   Created: 2023/01/11 16:11:26 by melprivi          #+#    #+#             */
+/*   Updated: 2023/01/24 23:02:00 by melprivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef     LIBFT_H
-# define    LIBFT_H
-# include	<unistd.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*ptrdst;
+	unsigned char	*ptrsrc;
+	size_t	  		i;
 
-#endif
+	if (!dst && !src)
+		return (NULL);
+	ptrdst = (unsigned char *)dst;
+	ptrsrc = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		ptrdst[i] = ptrsrc[i];
+		i++;
+	}
+	return (dst);
+}
