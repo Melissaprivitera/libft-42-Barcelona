0f9729d039a1e6bb684e6f61c05860b1bbb8e71e
Melissaprivitera <101509873+Melissaprivitera@users.noreply.github.com>
2023-04-26 17:03:51 +0200

    finished ft_substr with paco -s

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: melprivi <melprivi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:32:10 by melprivi          #+#    #+#             */
-/*   Updated: 2023/04/26 14:27:51 by melprivi         ###   ########.fr       */
+/*   Updated: 2023/04/26 17:03:45 by melprivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int	i;
 
 	i = 0;
+	
+	if ((start + len) > ft_strlen(s))
+		len = ft_strlen(s) - start;
 	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
-	if (len > ft_strlen(s))
-		len = (ft_strlen(s) - start);
-	substr = (char *)malloc(len + 1);
+	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (substr);
-	while (i < len && s)
+	while (i < len)
 	{
 		substr[i] = s[start + i];
 		i++;
